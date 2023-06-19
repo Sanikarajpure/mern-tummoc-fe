dfc098e114e1d388f1737eb6e09c8a8e9d3d807d
Sanika <sanika.rajpure@gmail.com>
2023-06-19 12:26:45 +0530

    integrated logout api

--- src/routes/Home/components/home.js
+++ src/routes/Home/components/home.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { getFileContents, writeToFile } from "../fileAction";
+import { userSignOut } from "../../Login/loginAction";
+import { useNavigate } from "react-router-dom";
 import { Oval } from "react-loader-spinner";
 
 const Home = () => {
   const user = useSelector((state) => state.User.loginInfo?.user);
-
+  const navigate = useNavigate();
   const [fileData, setFileData] = useState("");
   const [textData, setTextData] = useState("");
   const [loader, setLoader] = useState(false);
@@ -16,6 +18,17 @@ const Home = () => {
       setFileData(data);
       setTextData("");
       setLoader(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      let state = userSignOut();
+      if (state) {
+        navigate("/");
+      }
     } catch (error) {
       console.log(error);
     }
@@ -35,15 +48,29 @@ const Home = () => {
 
   return (
     <div className="text-light-call-sec dark:text-light-accent  items-center ">
-      <div className="flex w-full justify-center p-12">
-        Hello {user.firstName}
+      <div className="flex w-full  p-16">
+        <div className="p-2 flex-1 text-center text-xl">
+          Hello {user.firstName}
+        </div>
+
+        <div>
+          {" "}
+          <button
+            className=" pointer bg-light-call-sec p-2  text-white rounded-md "
+            onClick={() => {
+              handleLogout();
+            }}
+          >
+            Logout
+          </button>
+        </div>
       </div>
       <div className="flex w-full justify-around p-8">
         <div>
           <div className="py-2">Write to the File</div>
           <div>
             <input
-              className="p-2"
+              className="p-2 bg-dark-accent dark:bg-white"
               type="text"
               value={textData}
               onChange={(e) => {
@@ -70,7 +97,7 @@ const Home = () => {
         </div>
         <div className="  w-80 ">
           <div className="py-2">File contents</div>
-          <div className="bg-light-call-sec dark:bg-white h-80 p-2 whitespace-pre-line">
+          <div className="bg-dark-accent dark:bg-white h-80 p-2 whitespace-pre-line">
             {fileData ? fileData : "~Empty file~"}
           </div>
         </div>
